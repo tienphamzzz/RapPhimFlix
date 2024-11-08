c3f07b6803f1c7289c4971735d37731aab0e2569
Doanh <viaicamon28@gmail.com>
2024-11-08 20:13:15 +0700

    Them Cac class bo tro

--- RapPhimFlix/Appsetting/AppSetting.cs
+++ RapPhimFlix/Appsetting/AppSetting.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace RapPhimFlix.Appsetting
 {
-    internal class AppSetting
+    public class AppSetting
     {
+        public static string ConnectionString = "Data Source=LAPTOP-A56O33LS\\SQLEXPRESS;Initial Catalog=QLCinema;Integrated Security=True;Encrypt=True;";
+        private readonly string Admin = "Admin123";
+        private readonly string Password = "Admin123";
+        public bool checkAdmin(string username, string password)
+        {
+            if (username == null || password == null) return false;
+            if (username != Admin) return false;
+            if (password != Password) return false;
+            return true;
+        }
+        public static int VaiTro; // 0 là Admin
     }
 }
